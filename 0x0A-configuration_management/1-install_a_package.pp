627c510fd0bfdffa268ebbb5fd3130d610d042dd
SH-Codes <wldrms@outlook.com>
2024-03-28 21:53:07 -0700

    1-install_a_package.pp - install flask 2.1.0 from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 
-package { 'python3-pip':
-  ensure => present,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
