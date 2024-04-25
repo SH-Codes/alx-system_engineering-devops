2f4569d266190525b7fe5bfa6aa0c9c47f88534f
SH-Codes <wldrms@outlook.com>
2024-04-25 04:16:48 +0200

    0-gather_data_from_an_API.py - given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,43 +5,21 @@
 import requests
 from sys import argv
 
-
-def get_user_data(user_id):
-    endpoint = "https://jsonplaceholder.typicode.com/"
-    try:
-        user_response = requests.get(
-            endpoint + "users/{}".format(user_id), verify=False)
-        # Raise an error for HTTP status codes indicating failure
-        user_response.raise_for_status()
-        user = user_response.json()
-
-        todo_response = requests.get(
-            endpoint +
-            "todos?userId={}".format(user_id),
-            verify=False)
-        # Raise an error for HTTP status codes indicating failure
-        todo_response.raise_for_status()
-        todo = todo_response.json()
-
-        return user, todo
-
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return None
-
-
 if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Usage: python script.py <user_id>")
-        exit(1)
-
-    user_id = argv[1]
-    user_data = get_user_data(user_id)
-    if user_data:
-        user, todo = user_data
-        completed_tasks = [task['title']
-                           for task in todo if task.get('completed')]
-        print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-              len(completed_tasks), len(todo)))
-        for task in completed_tasks:
-            print("\t", task)
+    endpoint = "https://jsonplaceholder.typicode.com/"
+    userId = argv[1]
+    user = requests.get(
+        endpoint +
+        "users/{}".format(userId),
+        verify=False).json()
+    todo = requests.get(
+        endpoint +
+        "todos?userId={}".format(userId),
+        verify=False).json()
+    completed_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+          len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
