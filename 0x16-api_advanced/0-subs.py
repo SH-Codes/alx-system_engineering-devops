304c2b8997e4624cbcf8063b1c7c6f7e82357263
SH-Codes <wldrms@outlook.com>
2024-05-08 02:47:16 +0200

    0-subs.py - ueries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,25 +3,34 @@
 number of subscribers for a given subreddit
 """
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
+    Function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
-    """
 
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers of the subreddit. Returns 0 if subreddit
+             is None or not a string, or if there was an error retrieving the
+             data.
+    """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'Mozilla/5.0'}  # Changed user agent to a more generic one
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
+    response = requests.get(url, headers=user_agent)
+
+    if response.status_code != 200:  # Check if request was successful
+        return 0
 
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    except KeyError:
         return 0
