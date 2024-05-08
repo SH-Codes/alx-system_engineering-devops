d5dd23b0619759d48bbf2653d95c97a81971244b
SH-Codes <wldrms@outlook.com>
2024-05-08 02:52:49 +0200

    0-subs.py - ueries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,29 @@
-#!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers of the subreddit. Returns 0 if subreddit
+             is None, not a string, or if there was an error retrieving the data.
+    """
+    if not subreddit or not isinstance(subreddit, str):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    try:
+        response = requests.get(url, headers=user_agent, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
