fc6d010651daf6817d14bff1bc11f7d01dabf8d5
SH-Codes <wildrms@outlook.com>
2024-05-25 22:01:37 +0000

    0-subs.py - queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""number of subscribers for a given subreddit"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-
-    """
-    function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given
-    subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Microsoft Edge Version 124.0.2478.67'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-    except Exception:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
