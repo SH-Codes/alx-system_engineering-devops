0af06ddda5985331862bff830b8d6bf6bd2c5eb0
SH-Codes <wldrms@outlook.com>
2024-08-22 23:06:33 +0200

    tacking untracked files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,39 @@
-#!/usr/bin/python5
+#!/usr/bin/python3
 
-"""prints the titles of the first 10 hot posts listed for a given subreddit
-"""
+"""Prints the titles of the first 10 hot posts listed for a given subreddit"""
 
 from requests import get
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
+    Function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
     user_agent = {'User-agent': 'Microsoft Edge Version 124.0.2478.67'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    
     try:
-        my_data = results.get('data').get('children')
-        for i in my_data:
-            print(i.get('data').get('title'))
-    except Exception:
-        print("None")
+        response = get(url, headers=user_agent, params=params, timeout=10)
+        # Check for valid response
+        if response.status_code != 200:
+            print("None")
+            return
+        
+        results = response.json()
+        my_data = results.get('data', {}).get('children', [])
 
+        if not my_data:
+            print("None")
+            return
+        
+        for post in my_data:
+            print(post.get('data', {}).get('title', "None"))
+    except Exception as e:
+        print("None")
